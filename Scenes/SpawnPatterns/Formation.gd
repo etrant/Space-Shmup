20557342119afcfff6ad1957741bb68dfa523623
Ethan <30396562+etrant1@users.noreply.github.com>
2023-11-21 21:55:03 -0500

    Fix zig zag spawner logic

--- Scenes/SpawnPatterns/Formation.gd
+++ Scenes/SpawnPatterns/Formation.gd
@@ -1,9 +1,12 @@
 extends Node2D
 class_name Formation
 
+
 @onready var follower = preload("res://Scripts/Enemy/follower_basic_enemy.tscn")
 @onready var number_of_enemies : int = 10
 @export var required_enemies : int = 10 # 1 already in scene
+var finished_spawning = false
+var can_spawn = true
 
 
 func _ready() -> void:
@@ -11,7 +14,24 @@ func _ready() -> void:
 	
 	
 func _physics_process(delta) -> void:
-	if number_of_enemies == 0:
-		await get_tree().create_timer(2.0).timeout
+	if required_enemies > 0 and can_spawn:
+		spawn_enemy()
+		
+	if required_enemies == 0 and !finished_spawning:
+		finished_spawning = true
+		
+	if $Path2D.get_child_count() == 0 and finished_spawning:
+		await get_tree().create_timer(1.0).timeout
 		queue_free()
 	
+
+func spawn_enemy():
+	var f = follower.instantiate()
+	$Path2D.add_child(f)
+	required_enemies -= 1
+	can_spawn = false
+	$SpawnTimer.start()
+
+
+func _on_spawn_timer_timeout():
+	can_spawn = true
