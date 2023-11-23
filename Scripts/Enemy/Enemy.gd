1c7052d778bead0f6429cb63ada82ee7404c4813
Ethan <30396562+etrant1@users.noreply.github.com>
2023-11-22 19:27:25 -0500

    Fix player getting rewarded twice for kills
    Add hit value to enemies (default: 0)

--- Scripts/Enemy/Enemy.gd
+++ Scripts/Enemy/Enemy.gd
@@ -7,18 +7,19 @@ class_name Enemy
 @export var can_shoot = false
 @export var can_die = false
 @export var value = 100
+@export var hit_value = 0
 
 const EnemyDeathEffect = preload("res://Effects/enemy_death_effect.tscn")
 
 
 func hit(damage):
-	if can_die:
+	if can_die and health > 0:
 		var old_health = health
 		health -= damage
-		
+		Global.score += hit_value
 		print("Old: %s  New: %s"%[old_health, health])
 		
-		if health <= 0:
+		if health == 0:
 			die()
 			
 		hit_player.play('hit_flash')
